d07afe059ce037da3309dd49c6b992bc5e0964d3
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-28 23:42:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+let form=document.querySelector("form");
+form.addEventListener("submit",(e)=>{
+	e.preventDefault();
+	let Name=document.getElementById("name").value;
+	let Year=document.getElementById("year").value;
+	let h3=document.getElementById("url");
+
+	h3.innerText+=`name=${Name}&year=${Year}`
+})
